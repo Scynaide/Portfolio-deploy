99a8b44260248cfb892f580bb76ecea7ccb061ff
Scynaide <aron.turowski@dci-student.org>
2024-10-14 13:00:17 +0200

    added new files

---
+++ index.js
@@ -0,0 +1,58 @@
+// write to the console
+console.log("Hello from the JavaScript file")
+
+
+// variable
+let myName = "My name is Aron"
+
+console.log(myName)
+
+// camelCase my main city
+
+myMainCity = "I live in Berlin"
+
+// number
+let myFavNumber = "21"
+
+
+
+
+
+// String
+
+let myNewString = "Java is differnt than JavaScript"
+
+
+
+let lenght = "32"
+
+console.log(length)
+
+console.log(myNewString.toUpperCase())
+
+console.log(myNewString.slice(0,12))
+
+console.log(myNewString.includes("JavaScript"))
+
+
+
+if(myFavNumber >20)(
+    console.log("21 is a positive number")
+                    )
+
+console.log(21%2)
+console.log(21%3)
+console.log(21%4)
+console.log(21%5)
+console.log(21%6)
+console.log(21%7)
+
+console.log("1".localeCompare("1"))
+console.log("2".localeCompare("2"))
+console.log("3".localeCompare("3"))
+
+const numbers = [1, 2, 3];
+console.log(Math.max(...numbers)); // Output: 3
+
+
+
